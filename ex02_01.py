f8d71a4b703f17ba8ee6ef6cee10bc297cc2d79a
LeMinhHieu204 <sadodragon1597538426@gmail.com>
2025-03-08 08:41:22 +0700

    [add] lab-01 ex03

--- ex02_01.py
+++
@@ -1,5 +0,0 @@
-#Nhap ten va tuoi nguoi dung
-ten = input("Nhap ten cua ban: ")
-tuoi = input("Nhap tuoi cua ban: ")
-#In thong diep chao mung thong tin vua dang nhap
-print("Chao mung,",ten, "! Ban",tuoi, "tuoi.")
